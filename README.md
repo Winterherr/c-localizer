490caffa371135823327ae06f07ef0c756312d29
Tim Aldinger <tim.aldinger04@gmail.com>
2023-07-06 13:19:14 +0200

    added files

---
+++ README.md
@@ -0,0 +1,3 @@
+## Localizer
+Localizer is a small project written in C which is supposed to help users with localization in their own projects.
+The project is still in it's beginning. Featuring currently only a basic usage of the localization
---
+++ localizer.h
@@ -0,0 +1,35 @@
+#ifndef LOCALIZER_H_
+#define LOCALIZER_H_
+
+#include <linux/limits.h>
+#include <stdio.h>
+
+#define TITLE 0
+#define DESCRIPTION 1
+
+
+
+//extern char language_index_path[PATH_MAX];
+
+//inits
+
+void init_localizer(char* path, char* index_file_name);
+
+//Should be ../locale/
+void set_localizer_path(char* path);
+void open_language_index_file(char* index_file_name);
+void free_line_buffer(void);
+void append_localised_file_path(char* dest ,char* name, char language_id);
+void strcpy_to_char(char* dest, char* src, char stop);
+void close_files(void);
+//return de_ or en_ ... return should be checked for NULL ; check language_index.yaml for language ids
+char* get_language_id(char language);
+//get a localized string from any file
+char* get_localized_string_from_file(FILE* localized_file ,char* name, char category);
+// get a localized string from a the SET_FILE
+char* get_localized_string(char* name, char category);
+char* get_category(int category);
+//name= main_menu / description
+FILE* open_localised_file(char* name, char language_id);
+
+#endif
